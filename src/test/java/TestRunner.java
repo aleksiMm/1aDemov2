845d4b629cb1085d4e154c665b9db28a2e0c9617
a.sviridovics <a.sviridovics@googlemail.com>
2022-04-25 21:32:28 +0300

    *minor changes

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = "pretty",
-        features = {"C:\\Users\\a.sviridovics\\IdeaProjects\\demo-withall\\src\\test\\resources\\features"},
+        features = {"src/test/resources/features"},
         glue = {"steps"},
         tags = "@shopTest"
 )
